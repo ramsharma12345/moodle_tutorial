cb2af1b33bef7eaf5449541c1b996318badf01e4
ramsharma12345 <111233607+ramsharma12345@users.noreply.github.com>
2023-09-09 22:52:41 +0530

    Create p3.php
    Define an array variable as follows:
    
    $capitals = array('USA' => "Washington",
    'Great Britain' => "London",
    'New Zealand' => "Wellington",
    'Australia' => "Canberra",
    'Italy' => "Rome"
    'Canada' => "Ottawa");
    
    Write a program using repetition (loop) to display the capital values of each country, every country, as follows.
    
    Capital of USA is Washington.

---
+++ p3.php
@@ -0,0 +1,27 @@
+<?php 
+
+$capitals = array('USA' => "Washington",
+'Great Britain' => "London",
+'New Zealand' => "Wellington",
+'Australia' => "Canberra",
+'Italy' => "Rome",
+'Canada' => "Ottawa");
+
+foreach($capitals as $kay => $value){
+    echo "Capital of ".$kay." is ".$value.".<br>";
+}
+
+/*
+Define an array variable as follows:
+
+$capitals = array('USA' => "Washington",
+'Great Britain' => "London",
+'New Zealand' => "Wellington",
+'Australia' => "Canberra",
+'Italy' => "Rome"
+'Canada' => "Ottawa");
+
+Write a program using repetition (loop) to display the capital values of each country, every country, as follows.
+
+Capital of USA is Washington.
+*/
